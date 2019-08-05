c7c70f3e3721df328d6c158b24d4169e188d4b44
Lukasz Janyst <lukasz@jany.st>
2019-08-05 09:25:52 +0200

    tests: Test infix operator priority

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -95,3 +95,166 @@ false;
 
 	parseAndCompareAst(t, input, &expected)
 }
+
+func TestInfixPriority(t *testing.T) {
+	input := `10 + 2;
+3 * 20;
+10 + 2 * 6;
+12 * 7 + 12;
+12 * 7 + 12 * 8;
+2 + 4 * 5 * 6 * 7;
+-12 * 7 + 12 * -8;
+`
+	expected := ProgramNode{
+		[]Node{
+			&InfixNode{
+				token.Token{token.PLUS, "+", 1, 4},
+				&IntNode{
+					token.Token{token.INT, "10", 1, 1},
+					10,
+				},
+				&IntNode{
+					token.Token{token.INT, "2", 1, 6},
+					2,
+				},
+			},
+			&InfixNode{
+				token.Token{token.ASTERISK, "*", 2, 3},
+				&IntNode{
+					token.Token{token.INT, "3", 2, 1},
+					3,
+				},
+				&IntNode{
+					token.Token{token.INT, "20", 2, 5},
+					20,
+				},
+			},
+			&InfixNode{
+				token.Token{token.PLUS, "+", 3, 4},
+				&IntNode{
+					token.Token{token.INT, "10", 3, 1},
+					10,
+				},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 3, 8},
+					&IntNode{
+						token.Token{token.INT, "2", 3, 6},
+						2,
+					},
+					&IntNode{
+						token.Token{token.INT, "6", 3, 10},
+						6,
+					},
+				},
+			},
+			&InfixNode{
+				token.Token{token.PLUS, "+", 4, 8},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 4, 4},
+					&IntNode{
+						token.Token{token.INT, "12", 4, 1},
+						12,
+					},
+					&IntNode{
+						token.Token{token.INT, "7", 4, 8},
+						7,
+					},
+				},
+				&IntNode{
+					token.Token{token.INT, "12", 4, 10},
+					12,
+				},
+			},
+			&InfixNode{
+				token.Token{token.PLUS, "+", 5, 8},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 5, 4},
+					&IntNode{
+						token.Token{token.INT, "12", 5, 1},
+						12,
+					},
+					&IntNode{
+						token.Token{token.INT, "7", 5, 8},
+						7,
+					},
+				},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 5, 13},
+					&IntNode{
+						token.Token{token.INT, "12", 5, 10},
+						12,
+					},
+					&IntNode{
+						token.Token{token.INT, "8", 5, 15},
+						8,
+					},
+				},
+			},
+			&InfixNode{
+				token.Token{token.PLUS, "+", 6, 3},
+				&IntNode{
+					token.Token{token.INT, "2", 6, 1},
+					2,
+				},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 6, 7},
+					&IntNode{
+						token.Token{token.INT, "4", 6, 5},
+						4,
+					},
+					&InfixNode{
+						token.Token{token.ASTERISK, "*", 6, 11},
+						&IntNode{
+							token.Token{token.INT, "5", 6, 9},
+							5,
+						},
+						&InfixNode{
+							token.Token{token.ASTERISK, "*", 6, 15},
+							&IntNode{
+								token.Token{token.INT, "6", 6, 13},
+								6,
+							},
+							&IntNode{
+								token.Token{token.INT, "7", 6, 17},
+								7,
+							},
+						},
+					},
+				},
+			},
+			&InfixNode{
+				token.Token{token.PLUS, "+", 7, 9},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 7, 5},
+					&PrefixNode{
+						token.Token{token.MINUS, "-", 7, 1},
+						&IntNode{
+							token.Token{token.INT, "12", 7, 2},
+							12,
+						},
+					},
+					&IntNode{
+						token.Token{token.INT, "7", 7, 9},
+						7,
+					},
+				},
+				&InfixNode{
+					token.Token{token.ASTERISK, "*", 7, 14},
+					&IntNode{
+						token.Token{token.INT, "12", 7, 11},
+						12,
+					},
+					&PrefixNode{
+						token.Token{token.MINUS, "-", 7, 16},
+						&IntNode{
+							token.Token{token.INT, "8", 5, 17},
+							8,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	parseAndCompareAst(t, input, &expected)
+}
