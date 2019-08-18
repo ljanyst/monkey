95eabca6604aff5105255c8ecb2659145a701c1e
Lukasz Janyst <lukasz@jany.st>
2019-08-18 15:27:56 +0200

    tests: Test simple assignments

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -4,30 +4,55 @@ import (
 	"testing"
 )
 
-func evaluateAndCompareResult(t *testing.T, input []string, expected []Object) bool {
+func evaluateAndCompareResult(t *testing.T, input []string, expected []Object,
+	sideEffects []map[string]Object) bool {
 
 	if len(input) != len(expected) {
 		t.Errorf("Input and expected sizes differ")
 		return false
 	}
 
+	status := true
+
 	for i := 0; i < len(input); i++ {
 		c := NewContext()
 		obj, err := EvalString(input[i], c)
 		if err != nil {
-			t.Errorf("Unable to evaluate program %d: %s", i, err)
+			t.Errorf("[test %d] Unable to evaluate program: %s", i, err)
+			status = false
 			continue
 		}
 
 		exp := expected[i]
 
 		if obj.Type() != exp.Type() || obj.Value() != exp.Value() {
-			t.Errorf("Wrong result for test %d: %q. Expected %v, got %v", i, input[i], exp, obj)
+			t.Errorf("[test %d] Wrong result: %q. Expected %v, got %v", i, input[i], exp, obj)
+			status = false
 			continue
+		}
+
+		if len(sideEffects) == 0 {
+			continue
+		}
+
+		se := sideEffects[i]
+		for k, v := range se {
+			obj, err := c.Resolve(k)
+			if err != nil {
+				t.Errorf("[test %d] Expected to find variable %q but found none", i, v)
+				status = false
+				continue
+			}
+
+			if obj.Type() != v.Type() || obj.Value() != v.Value() {
+				t.Errorf("[test %d] Wrong object in variable %q. Expected %v, got %v", i, k, v, obj)
+				status = false
+				continue
+			}
 		}
 	}
 
-	return true
+	return status
 }
 
 func TestLiteralsAndIdentifiers(t *testing.T) {
@@ -49,7 +74,7 @@ func TestLiteralsAndIdentifiers(t *testing.T) {
 		&IntObject{-10},
 	}
 
-	evaluateAndCompareResult(t, input, expected)
+	evaluateAndCompareResult(t, input, expected, []map[string]Object{})
 }
 
 func TestInfixPriority(t *testing.T) {
@@ -79,5 +104,30 @@ func TestInfixPriority(t *testing.T) {
 		&IntObject{-16},
 	}
 
-	evaluateAndCompareResult(t, input, expected)
+	evaluateAndCompareResult(t, input, expected, []map[string]Object{})
+}
+
+func TestSimpleAssignments(t *testing.T) {
+	input := []string{
+		"let test1 = 12;",
+		"let test2 = 12 * 7 + 12 * 8;",
+		"let test3 = -12 * 7 == 12 + -8;",
+		"let test4 = 12; test4 = 22; test4 - 1;",
+	}
+
+	expected := []Object{
+		&IntObject{12},
+		&IntObject{180},
+		&BoolObject{false},
+		&IntObject{21},
+	}
+
+	sideEffects := []map[string]Object{
+		map[string]Object{"test1": &IntObject{12}},
+		map[string]Object{"test2": &IntObject{180}},
+		map[string]Object{"test3": &BoolObject{false}},
+		map[string]Object{"test4": &IntObject{22}},
+	}
+
+	evaluateAndCompareResult(t, input, expected, sideEffects)
 }
