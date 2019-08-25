b22116d384bd50b77b3814bd21c6ded7a78fcefc
Lukasz Janyst <lukasz@jany.st>
2019-08-25 18:29:05 +0200

    evaluator: Evaluate string slicing and concatenation

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -25,15 +25,37 @@ func EvalString(code string, c *Context, name string) (Object, error) {
 	return EvalReader(strings.NewReader(code), c, name)
 }
 
-func mkErrUnexpectedType(exp, got ObjectType, node parser.Node) error {
+func mkErrWrongType(exp, got ObjectType, node parser.Node) error {
 	return fmt.Errorf("%s Eval error: Expected type %s, got %s for expression %q",
 		node.Token().Location(), exp, got, node.String(""),
 	)
 }
 
+func mkErrWrongTypeStr(exp string, got ObjectType, node parser.Node) error {
+	return fmt.Errorf("%s Eval error: Expected type %s, got %s for expression %q",
+		node.Token().Location(), exp, got, node.String(""),
+	)
+}
+
+func mkErrWrongOpForType(tok lexer.Token, objType ObjectType) error {
+	return fmt.Errorf("%s Eval error: Invalid operator %s for type %s", tok.Location(), tok.Literal, objType)
+}
+
+func mkErrIndexOutOfBounds(node parser.Node, value, first, last int64) error {
+	tok := node.Token()
+	return fmt.Errorf("%s Eval error: Index %q is out of bounds: %d, valid range [%d:%d]",
+		tok.Location(), node.String(""), value, first, last,
+	)
+}
+
+func mkErrSliceEmpty(node parser.Node) error {
+	tok := node.Token()
+	return fmt.Errorf("%s Eval error: Slicing empty container %q", tok.Location(), node.String(""))
+}
+
 func mkErrWrongToken(expected string, got lexer.Token) error {
 	lit := got.Literal
-	return fmt.Errorf("%s Eval error: expected %s, got %q", got.Location(), expected, lit)
+	return fmt.Errorf("%s Eval error: Expected %s, got %q", got.Location(), expected, lit)
 }
 
 func evalBlock(node parser.Node, c *Context) (Object, error) {
@@ -98,14 +120,14 @@ func evalPrefix(node parser.Node, c *Context) (Object, error) {
 
 	if node.Token().Type == lexer.BANG {
 		if obj.Type() != BOOL {
-			return nil, mkErrUnexpectedType(BOOL, obj.Type(), exp)
+			return nil, mkErrWrongType(BOOL, obj.Type(), exp)
 		}
 		return &BoolObject{!obj.(*BoolObject).Value}, nil
 	}
 
 	if node.Token().Type == lexer.MINUS {
 		if obj.Type() != INT {
-			return nil, mkErrUnexpectedType(INT, obj.Type(), exp)
+			return nil, mkErrWrongType(INT, obj.Type(), exp)
 		}
 		return &IntObject{-obj.(*IntObject).Value}, nil
 	}
@@ -135,35 +157,17 @@ func evalAssign(node parser.Node, c *Context) (Object, error) {
 	return obj, nil
 }
 
-func evalInfix(node parser.Node, c *Context) (Object, error) {
-	iNode := node.(*parser.InfixNode)
-
-	if node.Token().Type == lexer.ASSIGN {
-		return evalAssign(node, c)
+func evalInfixString(op lexer.Token, lVal, rVal string) (Object, error) {
+	if op.Type == lexer.PLUS {
+		return &StringObject{lVal + rVal}, nil
 	}
 
-	left, err := EvalNode(iNode.Left, c)
-	if err != nil {
-		return nil, err
-	}
+	return nil, mkErrWrongOpForType(op, STRING)
 
-	right, err := EvalNode(iNode.Right, c)
-	if err != nil {
-		return nil, err
-	}
+}
 
-	if left.Type() != INT {
-		return nil, mkErrUnexpectedType(INT, left.Type(), iNode.Left)
-	}
-
-	if right.Type() != INT {
-		return nil, mkErrUnexpectedType(INT, right.Type(), iNode.Right)
-	}
-
-	lVal := left.(*IntObject).Value
-	rVal := right.(*IntObject).Value
-
-	switch node.Token().Type {
+func evalInfixInt(op lexer.Token, lVal, rVal int64) (Object, error) {
+	switch op.Type {
 	case lexer.PLUS:
 		return &IntObject{lVal + rVal}, nil
 	case lexer.MINUS:
@@ -186,7 +190,39 @@ func evalInfix(node parser.Node, c *Context) (Object, error) {
 		return &BoolObject{lVal != rVal}, nil
 	}
 
-	return nil, fmt.Errorf("Unrecognized token for infix expression: %s", node.Token().Literal)
+	return nil, mkErrWrongOpForType(op, INT)
+}
+
+func evalInfix(node parser.Node, c *Context) (Object, error) {
+	iNode := node.(*parser.InfixNode)
+	tok := node.Token()
+
+	if tok.Type == lexer.ASSIGN {
+		return evalAssign(node, c)
+	}
+
+	left, err := EvalNode(iNode.Left, c)
+	if err != nil {
+		return nil, err
+	}
+
+	right, err := EvalNode(iNode.Right, c)
+	if err != nil {
+		return nil, err
+	}
+
+	if left.Type() != INT && left.Type() != STRING {
+		return nil, mkErrWrongTypeStr("INT or STRING", left.Type(), iNode.Left)
+	}
+
+	if right.Type() != right.Type() {
+		return nil, mkErrWrongType(left.Type(), right.Type(), iNode.Right)
+	}
+
+	if left.Type() == STRING {
+		return evalInfixString(tok, left.(*StringObject).Value, right.(*StringObject).Value)
+	}
+	return evalInfixInt(tok, left.(*IntObject).Value, right.(*IntObject).Value)
 }
 
 func evalLet(node parser.Node, c *Context) (Object, error) {
@@ -226,12 +262,13 @@ func evalReturn(node parser.Node, c *Context) (Object, error) {
 }
 
 func evalStatement(node parser.Node, c *Context) (Object, error) {
-	if node.Token().Type == lexer.LET {
+	tok := node.Token()
+	if tok.Type == lexer.LET {
 		return evalLet(node, c)
 	} else if node.Token().Type == lexer.RETURN {
 		return evalReturn(node, c)
 	}
-	return nil, fmt.Errorf("Unrecognized statement: %s", node.Token().Literal)
+	return nil, fmt.Errorf("%s Eval error: Unrecognized statement: %s", tok.Location(), tok.Literal)
 }
 
 func evalConditional(node parser.Node, c *Context) (Object, error) {
@@ -243,7 +280,7 @@ func evalConditional(node parser.Node, c *Context) (Object, error) {
 	}
 
 	if condObj.Type() != BOOL {
-		return nil, mkErrUnexpectedType(BOOL, condObj.Type(), condNode.Condition)
+		return nil, mkErrWrongType(BOOL, condObj.Type(), condNode.Condition)
 	}
 
 	if condObj.(*BoolObject).Value {
@@ -282,7 +319,7 @@ func evalFunctionCall(node parser.Node, c *Context) (Object, error) {
 	}
 
 	if fObj.Type() != FUNCTION {
-		return nil, mkErrUnexpectedType(FUNCTION, fObj.Type(), funcCallNode.Name)
+		return nil, mkErrWrongType(FUNCTION, fObj.Type(), funcCallNode.Name)
 	}
 
 	f := fObj.(*FunctionObject)
@@ -312,6 +349,70 @@ func evalFunctionCall(node parser.Node, c *Context) (Object, error) {
 		return retObj.(*ReturnObject).Value, nil
 	}
 	return retObj, nil
+}
+
+func evalSlice(node parser.Node, c *Context) (Object, error) {
+	sliceNode := node.(*parser.SliceNode)
+
+	tok := sliceNode.Subject.Token()
+	if tok.Type != lexer.IDENT {
+		return nil, mkErrWrongToken("identifier", tok)
+	}
+	name := sliceNode.Subject.(*parser.IdentifierNode).Value
+
+	sliceObj, err := c.Resolve(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if sliceObj.Type() != STRING {
+		return nil, mkErrWrongType(STRING, sliceObj.Type(), sliceNode.Subject)
+	}
+
+	runes := []rune(sliceObj.(*StringObject).Value)
+	length := int64(len(runes))
+
+	if length == 0 {
+		return nil, mkErrSliceEmpty(sliceNode.Subject)
+	}
+
+	startObj, err := EvalNode(sliceNode.Start, c)
+	if err != nil {
+		return nil, err
+	}
+
+	if startObj.Type() != INT {
+		return nil, mkErrWrongType(INT, startObj.Type(), sliceNode.Start)
+	}
+	start := startObj.(*IntObject).Value
+
+	if start < 0 || start >= length {
+		return nil, mkErrIndexOutOfBounds(sliceNode.Start, start, 0, length-1)
+	}
+
+	if sliceNode.End != nil {
+		endObj, err := EvalNode(sliceNode.End, c)
+		if err != nil {
+			return nil, err
+		}
+
+		if endObj.Type() != INT {
+			return nil, mkErrWrongType(INT, endObj.Type(), sliceNode.End)
+		}
+
+		end := endObj.(*IntObject).Value
+		if end < 0 || end > length {
+			return nil, mkErrIndexOutOfBounds(sliceNode.End, end, 0, length)
+		}
+
+		if start > end {
+			return nil, mkErrIndexOutOfBounds(sliceNode.End, end, start, length)
+		}
+
+		return &StringObject{string(runes[start:end])}, nil
+	}
+
+	return &RuneObject{runes[start]}, nil
 }
 
 func EvalNode(node parser.Node, c *Context) (Object, error) {
@@ -346,6 +447,8 @@ func EvalNode(node parser.Node, c *Context) (Object, error) {
 		return evalFunction(node, c)
 	case *parser.FunctionCallNode:
 		return evalFunctionCall(node, c)
+	case *parser.SliceNode:
+		return evalSlice(node, c)
 	default:
 		return nil,
 			fmt.Errorf("%s Eval error: Evaluator not implemented for %s",
